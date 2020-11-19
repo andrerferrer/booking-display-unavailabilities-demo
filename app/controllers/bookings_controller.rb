f184737d1c71dd3a013a701e4e3e9ff21611fdbc
andre ferrer <andrerferrer@hotmail.com>
2020-11-19 09:26:32 -0300

    adjust the controller to the new migration

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,26 @@
 class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
+    # have an empty booking for the form
+    @booking = Booking.new
   end
 
   def create
+    
     @booking = Booking.new(strong_params)
-    @booking.save ? redirect_to(bookings_path) : render(:index)
+    if @booking.save 
+      redirect_to(bookings_path) 
+    else 
+      # find all bookings to render index
+      @bookings = Booking.all
+      render(:index)
+    end
   end
 
   private
 
   def strong_params
-    params.require(:booking).permit(:booked_at, :restaurant_id)
+    params.require(:booking).permit(:start_on, :end_on, :restaurant_id)
   end
 
 end
