c3cdfc7ed5c0535bd9cd0311ff1039e723b68c23
andre ferrer <andrerferrer@hotmail.com>
2021-02-24 10:05:03 -0300

    handle dataset with flatpickr

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,9 +1,15 @@
 import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {
-    altInput: true
-  });
+  const newBookingForm = document.getElementById('new_booking');
+  if (newBookingForm) {
+    flatpickr(".datepicker", {
+      minDate: 'today', // make previous dates unavailable
+      altInput: true, // display a user friendlier message
+      dateFormat: "Y-m-d",
+      disable: JSON.parse(newBookingForm.dataset.unavailableDates)
+    });
+  }
 };
 
 export { initFlatpickr };
