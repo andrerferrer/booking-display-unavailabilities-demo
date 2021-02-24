ca32040e8a8ad5fe407192d68a08e754bc2a4bcb
andre ferrer <andrerferrer@hotmail.com>
2021-02-24 10:04:49 -0300

    add unavailable dates method to the restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,15 @@ class Restaurant < ApplicationRecord
     name
     address
   ]
+
   has_many :reviews
   has_many :bookings
+
+  def unavailable_dates
+    # If you want to read more
+    # https://medium.com/lightthefuse/ruby-on-rails-date-validation-in-a-booking-and-disabling-dates-in-date-picker-3e5b4e9b4640
+    bookings.pluck(:start_on, :end_on).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
